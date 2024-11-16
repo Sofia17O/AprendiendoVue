291286cc08a945ea5619c2d2e56c807538592292
Sofia17O <sofia.ortiz@sumimedical.com>
2024-11-16 07:53:51 -0500

    Se integra tailwidCSS

---
+++ src/components/MyCounter.vue
@@ -0,0 +1,31 @@
+<template>
+  <!-- Contador -->
+  <section>
+    <h3>Counter: {{ counter }}</h3>
+    <h3>Square: {{ squareCounter }}</h3>
+
+    <div>
+      <button class="btn" @click="counter++">+1</button>
+      <button class="btn" @click="counter--">-1</button>
+    </div>
+  </section>
+</template>
+
+<!-- Script con Composition -->
+
+<script lang="ts" setup>
+import { computed, ref } from 'vue';
+
+const props = defineProps({
+  value: { type: Number, required: true },
+});
+
+const counter = ref(props.value);
+const squareCounter = computed(() => counter.value * counter.value);
+</script>
+
+<style scope>
+.btn {
+  @apply p-5 bg-blue-500 rounded hover:bg-blue-300 mr-2;
+}
+</style>
